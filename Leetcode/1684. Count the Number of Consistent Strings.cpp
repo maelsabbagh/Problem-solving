2567c79c66c0b64d11645a308ff2accebe911a4f
Mohamed Amr <elsabbagh.mohamed98@gmail.com>
2021-03-26 16:50:34 +0200

    Add files via upload
    not optimized code

---
+++ Leetcode/1684. Count the Number of Consistent Strings.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int countConsistentStrings(string allowed, vector<string>& words)
+    {
+        int c=0;
+        for(int i=0;i<words.size();i++)
+        {
+            string s = words[i];
+            vector<bool>frq;
+            for(int j=0;j<s.size();j++)
+            {
+                frq.push_back(0);
+            }
+            for(int j=0;j<s.size();j++)
+            {
+                for(int k=0;k<allowed.size();k++)
+                {
+                    if(s[j]==allowed[k])
+                    {
+                        frq[j]=1;
+                    }
+                }
+            }
+            bool canBe=true;
+            for(int j=0;j<frq.size();j++)
+            {
+                if(!frq[j])
+                {
+                    canBe=false;
+                }
+            }
+            if (canBe)c++;
+        }
+        return c;
+    }
+};
